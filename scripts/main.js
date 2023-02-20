1df7e4def5218caa3f3bd11475935ecb7b942400
Darya Daneshmand <79486895+daryadaneshmand@users.noreply.github.com>
2023-02-20 11:43:33 -0800

    Scrollytelling 1

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+npm install scrollama intersection-observer --save
+
+import scrollama from "scrollama"; // or...
+const scrollama = require("scrollama");
+
+// instantiate the scrollama
+const scroller = scrollama();
+
+// setup the instance, pass callback functions
+scroller
+  .setup({
+    step: ".step",
+  })
+  .onStepEnter((response) => {
+    // { element, index, direction }
+  })
+  .onStepExit((response) => {
+    // { element, 1, down }
+  });
